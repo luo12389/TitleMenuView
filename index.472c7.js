4b3fbe600db9b4459c1070b72351d45645e48a3c
lc1995 <1224631767@qq.com>
2023-10-19 10:41:54 +0800

    项目提交

--- index.472c7.js
+++ index.472c7.js
@@ -1,4 +1,4 @@
-System.register(["./application.js"], function (_export, _context) {
+System.register(["./application.c7579.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
@@ -8,8 +8,8 @@ System.register(["./application.js"], function (_export, _context) {
   }
 
   return {
-    setters: [function (_applicationJs) {
-      Application = _applicationJs.Application;
+    setters: [function (_applicationC7579Js) {
+      Application = _applicationC7579Js.Application;
     }],
     execute: function () {
       canvas = document.getElementById('GameCanvas');
